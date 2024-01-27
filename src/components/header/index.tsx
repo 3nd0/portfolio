772d4843034bf1548a61bced9340b1b53c873ffc
wataru.endo <endo0430w@gmail.com>
2024-01-28 04:16:40 +0900

    ヘッダーのレイアウト変更

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,46 +3,66 @@ import Link from 'next/link'
 
 const Header = (): JSX.Element => {
   return (
-    <div className="flex flex-row justify-between content-center items-center border-b border-slate-600 h-20 px-8">
-      <div className="basis-1/2">
-        <h1 className="font-sans">Portfolio</h1>
+    <div className="flex flex-col gap-10">
+      <div className="mx-36 px-10 h-16">
+        <p className="font-sans text-3xl">Portfolio</p>
       </div>
-      <div className="basis-1/2 flex flex-row-reverse gap-4">
+      <div className="mx-72 px-10 flex flex-row">
         <div>
-          <Link href="https://github.com/3nd0/portfolio">
-            <Image
-              src="/logo/github.svg"
-              alt="Github Logo"
-              className="dark:invert"
-              width={35}
-              height={35}
-              priority
-            />
-          </Link>
+          <Image
+            src="/image/IMG_0850.jpg"
+            alt="ID photo"
+            width={120}
+            height={120}
+            priority
+            className="rounded-full"
+          />
         </div>
-        <div>
-          <Link href="https://www.instagram.com/w4.3nd0/">
-            <Image
-              src="/logo/instagram.svg"
-              alt="Github Logo"
-              className="dark:invert"
-              width={35}
-              height={35}
-              priority
-            />
-          </Link>
-        </div>
-        <div>
-          <Link href="https://twitter.com/home">
-            <Image
-              src="/logo/twitter.svg"
-              alt="Github Logo"
-              className="dark:invert"
-              width={35}
-              height={35}
-              priority
-            />
-          </Link>
+
+        {/* profile */}
+        <div className="flex flex-col pl-7 gap-4">
+          <div className="flex flex-row gap-4 items-end">
+            <p className="font-sans text-2xl font-bold">遠藤 航</p>
+            <p className="font-sans text-sm">Endo Wataru</p>
+          </div>
+          <p className="font-sans">生年月日: 1992/04/30</p>
+          <p className="font-sans">出身地: 千葉県</p>
+          <p className="font-sans">
+            趣味: 車, バイク, MTB, 釣り, キャンプ, 料理, 食べ歩き, ゲーム
+          </p>
+          {/* social icons */}
+          <div className="flex flex-row gap-4">
+            <Link href="https://github.com/3nd0/portfolio">
+              <Image
+                src="/logo/github.svg"
+                alt="Github Logo"
+                className="dark:invert"
+                width={35}
+                height={35}
+                priority
+              />
+            </Link>
+            <Link href="https://www.instagram.com/w4.3nd0/">
+              <Image
+                src="/logo/instagram.svg"
+                alt="Github Logo"
+                className="dark:invert"
+                width={35}
+                height={35}
+                priority
+              />
+            </Link>
+            <Link href="https://twitter.com/home">
+              <Image
+                src="/logo/twitter.svg"
+                alt="Github Logo"
+                className="dark:invert"
+                width={35}
+                height={35}
+                priority
+              />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
