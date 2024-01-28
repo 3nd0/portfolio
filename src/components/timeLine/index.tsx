74f8a7c12ee616ebc87be97246569e3240abdb42
wataru.endo <endo0430w@gmail.com>
2024-01-28 22:34:51 +0900

    タイムラインの上下の余白調整 (#7)

--- src/components/timeLine/index.tsx
+++ src/components/timeLine/index.tsx
@@ -7,7 +7,7 @@ const TimeLine = (props: { timeLine: TimeLine[] }): JSX.Element => {
     <ol className="relative border-s border-gray-200">
       {timeLine.map((contents, id) => (
         <Fragment key={id}>
-          <li className="mb-10 ms-4">
+          <li className="my-5 ms-4">
             <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white "></div>
             <time className="mb-1 text-sm font-normal leading-none text-gray-400 ">
               {contents.date}
