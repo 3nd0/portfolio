33d1aab6b943e5dfcc0a21120bfdb55ca6c529ed
wataru.endo <endo0430w@gmail.com>
2024-01-28 22:58:28 +0900

    Add anchor (#7)

--- src/components/timeLine/index.tsx
+++ src/components/timeLine/index.tsx
@@ -1,5 +1,6 @@
 import { Fragment } from 'react'
 import type { TimeLine } from './type'
+import Link from 'next/link'
 
 const TimeLine = (props: { timeLine: TimeLine[] }): JSX.Element => {
   const { timeLine } = props
@@ -25,6 +26,14 @@ const TimeLine = (props: { timeLine: TimeLine[] }): JSX.Element => {
             <p className="mb-4 text-base font-normal text-gray-500 ">
               {contents.description}
             </p>
+
+            {/* anchor */}
+            <Link
+              href="/detail"
+              className='"inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700'
+            >
+              Learn more
+            </Link>
           </li>
         </Fragment>
       ))}
