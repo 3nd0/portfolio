bef33a8c1bb413c136033c40024d63f381e3bb4a
wataru.endo <endo0430w@gmail.com>
2024-01-28 22:48:49 +0900

    Add comment (#7)

--- src/components/timeLine/index.tsx
+++ src/components/timeLine/index.tsx
@@ -8,13 +8,20 @@ const TimeLine = (props: { timeLine: TimeLine[] }): JSX.Element => {
       {timeLine.map((contents, id) => (
         <Fragment key={id}>
           <li className="my-5 ms-4">
+            {/* pointer */}
             <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white "></div>
+
+            {/* date section */}
             <time className="mb-1 text-sm font-normal leading-none text-gray-400 ">
               {contents.date}
             </time>
+
+            {/* title section */}
             <h3 className="text-lg font-semibold text-gray-900 text-gray">
               {contents.title}
             </h3>
+
+            {/* description section */}
             <p className="mb-4 text-base font-normal text-gray-500 ">
               {contents.description}
             </p>
