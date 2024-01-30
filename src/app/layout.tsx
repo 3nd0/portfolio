2322c37c7e3594677db5c305b28e9b2f46f88a5c
wataru.endo <endo0430w@gmail.com>
2024-01-31 01:35:05 +0900

    サイドバーを一旦、コメントアウト

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import SideBar from '@/components/sideBar'
+// import SideBar from '@/components/sideBar'
 
 export const metadata: Metadata = {
   title: 'Portfolio',
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body>
-        <SideBar />
+        {/* <SideBar /> */}
         {children}
       </body>
     </html>
